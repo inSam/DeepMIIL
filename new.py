9b3dc023a683daa66368aaf973538e42990d109e
heheggg <samx@stanford.edu>
2018-07-28 16:40:39 -0700

    save debug

--- new.py
+++ new.py
@@ -402,19 +402,22 @@ def save_images(fetches, step=None):
         os.makedirs(image_dir)
 
     filesets = []
-    for i, in_path in enumerate(fetches["paths"]):
-        name, _ = os.path.splitext(os.path.basename(in_path.decode("utf8")))
-        fileset = {"name": name, "step": step}
-        for kind in ["inputs", "outputs", "targets"]:
-            filename = name + "-" + kind + ".png"
-            if step is not None:
-                filename = "%08d-%s" % (step, filename)
-            fileset[kind] = filename
-            out_path = os.path.join(image_dir, filename)
-            contents = fetches[kind][i]
-            with open(out_path, "wb") as f:
-                f.write(contents)
-        filesets.append(fileset)
+
+
+    print(fetches["paths"])
+    # for i, in_path in enumerate(fetches["paths"]):
+    #     name, _ = os.path.splitext(os.path.basename(in_path.decode("utf8")))
+    #     fileset = {"name": name, "step": step}
+    #     for kind in ["inputs", "outputs", "targets"]:
+    #         filename = name + "-" + kind + ".png"
+    #         if step is not None:
+    #             filename = "%08d-%s" % (step, filename)
+    #         fileset[kind] = filename
+    #         out_path = os.path.join(image_dir, filename)
+    #         contents = fetches[kind][i]
+    #         with open(out_path, "wb") as f:
+    #             f.write(contents)
+    #     filesets.append(fileset)
     return filesets
 
 def append_index(filesets, step=False):
