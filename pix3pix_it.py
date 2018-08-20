de2d680a9d609beeaec2d57955b49ee026c7964c
heheggg <samx@stanford.edu>
2018-08-20 11:52:51 -0700

    bug

--- pix3pix_it.py
+++ pix3pix_it.py
@@ -406,7 +406,7 @@ def save_images(fetches, step=None):
             for j in range(a.slice_size):
                 filefolder = str(name[0]) + "-" + str(name[1] + j)
                 num = min(a.slice_size, name[1])
-                filename =  str(min(a.slice_size - j, num)) + "-" + "kind" + ".png"
+                filename =  str(min(a.slice_size - j, num)) + "-" + kind + ".png"
                 fold_path = os.path.join(image_dir, filefolder)
                 out_path = os.path.join(fold_path, filename)
                 if step is not None:
