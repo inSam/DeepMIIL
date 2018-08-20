12b5d825d2f1161049458f72e36f6778791d73a3
heheggg <samx@stanford.edu>
2018-08-20 14:36:55 -0700

    stuff

--- pix3pix_it.py
+++ pix3pix_it.py
@@ -406,7 +406,6 @@ def save_images(fetches, step=None):
         for kind in ["outputs", "inputs", "targets"]:
             contents = fetches[kind][i]
             for j in range(len(contents)):
-                print("content size: %d" % len(contents))
                 filefolder = str(name[0]) + "-" + str(name[1] + j)
                 num = min(a.slice_size, name[1])
                 filename =  str(min(a.slice_size - j - 1, num)) + "-" + kind + ".png"
